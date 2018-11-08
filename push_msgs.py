6dfc3e55c33938b779863e62d20f508f07b4bfea
MiglyA <zq6295migly@gmail.com>
2018-11-08 10:48:32 +0900

    [add]push_msg追加

--- push_msgs.py
+++ push_msgs.py
@@ -11,11 +11,12 @@
 #  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #  License for the specific language governing permissions and limitations
 #  under the License.
-
+import json
 import os
 import sys
 from argparse import ArgumentParser
 
+import requests
 from flask import Flask, request, abort
 from linebot import (
     LineBotApi, WebhookHandler
@@ -43,39 +44,18 @@ line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
 
 
-@app.route("/callback", methods=['POST'])
-def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-        #handler.add(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-
-    return 'OK'
-
-
-@handler.add(MessageEvent, message=TextMessage)
-def message_text(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text)
-    )
-
-
-if __name__ == "__main__":
-    arg_parser = ArgumentParser(
-        usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
-    )
-    arg_parser.add_argument('-p', '--port', default=8000, help='port')
-    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
-    options = arg_parser.parse_args()
-
-    app.run(debug=options.debug, port=options.port)
+url = 'https://api.line.me/v2/bot/message/push'
+data = {
+    "to": "Ud1fe3db24904988cdc941eb50d5e4200",
+    "messages": [
+        {
+            "type": "text",
+            "text": "Hello, user!"
+        }
+    ]
+}
+headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer ' + channel_access_token
+}
+requests.post(url, data=json.dumps(data), headers=headers)
