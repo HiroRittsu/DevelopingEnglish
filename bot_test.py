de210e1322871269875fd8db9617a0e3c7b6edf8
MiglyA <zq6295migly@gmail.com>
2018-11-08 17:51:34 +0900

    [update]bot_test作成

--- bot_test.py
+++ bot_test.py
@@ -1,4 +1,23 @@
+import os
+import sys
+import time
+
 import LineApp as LA
 
 if __name__ == '__main__':
-    app = LA.LineApp()
+
+    # get channel_secret and channel_access_token from your environment variable
+    channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
+    channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
+    if channel_secret is None:
+        print('Specify LINE_CHANNEL_SECRET as environment variable.')
+        sys.exit(1)
+    if channel_access_token is None:
+        print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
+        sys.exit(1)
+
+    app = LA.LineApp(channel_secret,channel_access_token)
+
+    while True:
+        time.sleep(1)
+        print(app.receive)
