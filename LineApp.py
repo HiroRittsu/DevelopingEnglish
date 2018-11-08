408484e4a3c6dc808d0870ccbcb3cc7b9c4c4507
MiglyA <zq6295migly@gmail.com>
2018-11-08 19:20:31 +0900

    ラズパイ共有

--- LineApp.py
+++ LineApp.py
@@ -30,7 +30,28 @@ line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
 
 app = Flask(__name__)
-##################################3
+##################################
+
+
+@app.route("/callback", methods=['POST'])
+def callback(self):
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    print(json.loads(body))
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+        # handler.add(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
 
 class LineApp:
 
@@ -57,26 +78,6 @@ class LineApp:
         options = arg_parser.parse_args()
 
         app.run(debug=options.debug, port=options.port)
-
-    @app.route("/callback", methods=['POST'])
-    def callback(self):
-        # get X-Line-Signature header value
-        signature = request.headers['X-Line-Signature']
-
-        # get request body as text
-        body = request.get_data(as_text=True)
-        app.logger.info("Request body: " + body)
-
-        print(json.loads(body))
-
-        # handle webhook body
-        try:
-            handler.handle(body, signature)
-            # handler.add(body, signature)
-        except InvalidSignatureError:
-            abort(400)
-
-        return 'OK'
 
 
 
