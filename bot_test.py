abfb00065fc70d66c207617c6d3e72a9dbd11101
MiglyA <zq6295migly@gmail.com>
2018-11-09 09:35:01 +0900

    ラズパイ共有

--- bot_test.py
+++ bot_test.py
@@ -1,12 +1,18 @@
-import os
-import sys
 import time
-
 import LineApp as la
+
+id = ''
 
 if __name__ == '__main__':
 
+    app = la.LineApp()
+
     while True:
         time.sleep(1)
-        la.push_msgs("hello")
-        print(la.receive)
+        msg = app.get_msgs()
+
+        if not msg == '':
+            id = msg[0]
+
+        if not id == '':
+            app.push_msgs(id,'hello')
