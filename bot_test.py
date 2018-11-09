f29d4f67a4618830c9c88bcdc5b96ca92d1f1859
MiglyA <zq6295migly@gmail.com>
2018-11-09 09:43:21 +0900

    ラズパイ共有

--- bot_test.py
+++ bot_test.py
@@ -9,10 +9,8 @@ if __name__ == '__main__':
 
     while True:
         time.sleep(1)
-        msg = app.get_msgs()
+        #msg = app.get_msgs()
 
-        if not len(msg) == 0:
-            id = msg[0]
+        print(la.msgs)
 
-        if not id == '':
-            app.push_msgs(id,'hello')
+        app.push_msgs('U444d8a9ca45523b6fcda0226769d9983', 'hello')
