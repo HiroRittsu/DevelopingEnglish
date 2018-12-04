808ee35cfdf13bee4ca4f7f7b3fb3de26f1a410a
MiglyA <zq6295migly@gmail.com>
2018-12-04 12:32:09 +0900

    [update]main関数実装開始

---
+++ src/main.py
@@ -0,0 +1,24 @@
+import time
+
+import schedule
+from lib import LineApp
+
+app = LineApp.LineApp()
+
+
+def job():
+    print("job!")
+    app.push_msgs('U444d8a9ca45523b6fcda0226769d9983', 'start')
+
+
+schedule.every(10).seconds.do(job)
+
+
+def main():
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    main()
