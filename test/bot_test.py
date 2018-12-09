57e5984d4dab9686f7141fafcd059a58203663a9
MiglyA <zq6295migly@gmail.com>
2018-12-10 00:14:36 +0900

    ラズパイ共有

--- test/bot_test.py
+++ test/bot_test.py
@@ -13,10 +13,9 @@ if __name__ == '__main__':
     while True:
         time.sleep(1)
         # msg = app.get_msgs()
+        msg.append(app.get_msgs())
 
-        print((app.get_msgs()).pop)
-        print(len(app.get_msgs()))
-
+        print(msg.pop(0))
         # app.push_msgs('U444d8a9ca45523b6fcda0226769d9983', '尊い...っっ!!')
 
         # app.push_img('U444d8a9ca45523b6fcda0226769d9983', 'https://cdn.ibispaint.com/movie/372/896/372896921/image372896921.png')
