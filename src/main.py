bd8c333fe70f95ea71b7a5e70b289b6151ae2c47
MiglyA <zq6295migly@gmail.com>
2018-12-09 23:55:06 +0900

    ラズパイ共有

--- src/main.py
+++ src/main.py
@@ -17,7 +17,7 @@ userID = 'U444d8a9ca45523b6fcda0226769d9983'
 def answer():
     while len(app.get_msgs()) == 0:
         print('wait')
-
+        time.sleep(0.1)
     return app.get_msgs.pop(0)[1]
 
 
