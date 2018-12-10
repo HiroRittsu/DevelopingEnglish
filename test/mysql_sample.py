1b4041b5d00d8b7a4b9149ea37c748baf4821470
MiglyA <zq6295migly@gmail.com>
2018-12-10 10:50:20 +0900

    ラズパイ共有

---
+++ test/mysql_sample.py
@@ -0,0 +1,13 @@
+import sys
+
+sys.path.append('../lib/')
+from lib import ControlDB
+import random
+
+ControlDB.init('botDB')
+
+# print(len(ControlDB.select('select id from userdata')))
+
+# print(str(random.sample(ControlDB.select('select id from userdata'), 10)[0]).replace(',)', '').replace('(', ''))
+
+ControlDB.update('update userdata set answer_count=1,right_rate=2 where id=2')
