fa25813cb2495a7fca94e96e9831d91896aada2b
MiglyA <zq6295migly@gmail.com>
2018-12-10 18:36:26 +0900

    ラズパイ共有

--- src/main.py
+++ src/main.py
@@ -39,8 +39,9 @@ def job():
 
     count = 1
     for question_id in question_ids:
+        print(question_id[0][0])
         question = ControlDB.select(
-            'select * from words where id=' + str(question_id[0]))[0]
+            'select * from words where id=' + str(question_id[0][0]))[0]
 
         print(question)
 
