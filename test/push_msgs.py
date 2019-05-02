c104bf6847c840b4336a9a6fb632719c9bac0b15
MiglyA <zq6295migly@gmail.com>
2019-05-02 15:18:43 +0900

    ラズパイ共有

--- test/push_msgs.py
+++ test/push_msgs.py
@@ -17,33 +17,32 @@ import sys
 from argparse import ArgumentParser
 
 from flask import Flask, request, abort
+
 from linebot import (
-    LineBotApi, WebhookHandler
+	LineBotApi, WebhookHandler
 )
 from linebot.exceptions import (
-    InvalidSignatureError
+	InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+	MessageEvent, TextMessage, TextSendMessage,
 )
 
 app = Flask(__name__)
 
 # get channel_secret and channel_access_token from your environment variable
-channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
-channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
-if channel_secret is None:
-    print('Specify LINE_CHANNEL_SECRET as environment variable.')
-    sys.exit(1)
-if channel_access_token is None:
-    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
-    sys.exit(1)
+channel_secret = ''
+channel_access_token = ''
+with open('../token', 'r') as f:
+	for l in f.readlines():
+		if 'LINE_CHANNEL_SECRET' in l:
+			channel_secret = l.split(':')[1]
+		if 'LINE_CHANNEL_ACCESS_TOKEN' in l:
+			channel_access_token = l.split(':')[1]
 
 line_bot_api = LineBotApi(channel_access_token)
 
-
-
 line_bot_api.push_message(
-    'U444d8a9ca45523b6fcda0226769d9983',
-    TextSendMessage("Hello")
+	'U444d8a9ca45523b6fcda0226769d9983',
+	TextSendMessage("Hello")
 )
